6eb41cadae68c06fb2d99ad464489f6a07fc0704
Emin Demirci <emin@hazelcast.com>
2019-05-10 14:26:46 +0300

    New version of hazelcast-jet-enterprise

--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@ helm package ~/emindemirci/Development/ibm-charts/community/hazelcast-jet-enterp
 helm repo index --url https://eminn.github.io/hazelcast-charts/ .
 git add .
 git commit -m "New version of hazelcast-jet-enterprise" --no-edit
-git push origin gh-pages -f
+git push emin gh-pages -f
